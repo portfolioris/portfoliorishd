638c181768bf8450f80d9de432e4b301ac832c38
Hulsbosch <j.hulsbosch@colours.nl>
2017-11-16 21:16:01 +0100

    move functions into separate file

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { TimelineMax, Linear, Power1 } from 'gsap';
+import { TimelineMax, Linear, Power1, morphSVG } from 'gsap';
 import * as PIXI from 'pixi.js';
 import 'gsap/PixiPlugin';
 import 'pixi-display';
+import { moveAndLineTo, textMessage, placeFlash, placeVideo } from './helpers';
 
 import './Canvas.css';
 import imageFile from './../images/intro_1.jpg';
@@ -10,55 +11,25 @@ import videoFile from './../images/intro_seq_1.mp4';
 
 class Canvas extends Component {
   componentDidMount() {
-    function textMessage(text) {
-      const message = new PIXI.Text(
-        text,
-        {
-          fontFamily: 'TypeWriter',
-          fontSize: 64,
-          fill: 'white',
-        },
-      );
-
-      message.anchor.x = 0.5;
-      message.anchor.y = 0.5;
-      message.x = 1320;
-      message.y = 540;
-      message.alpha = 0;
-      app.stage.addChild(message);
-      return message;
-    }
-
-    function moveAndLineTo(shape, points) {
-      shape.clear();
-      shape.beginFill(0xFFFFFF);
-      shape.moveTo(points[0], points[1]);
-      for (let i = 2; i < points.length; i = i + 2) {
-        shape.lineTo(points[i], points[i + 1]);
-      }
-    }
-
-    function onAssetsLoaded(loader, resources) {
+    const onAssetsLoaded = (loader, resources) => {
+      // Setup layers. TopLayer is for the flash, which goes over other assets.
       app.stage.displayList = new PIXI.DisplayList();
-      const blueLayer = new PIXI.DisplayGroup(-1, true);
+      const topLayer = new PIXI.DisplayGroup(1, true);
 
       /* === SETUP FIRST IMAGE === */
       const imageRoom1 = new PIXI.Sprite(resources.imageRoom1.texture);
       imageRoom1.alpha = 0;
-      imageRoom1.displayGroup = blueLayer;
       app.stage.addChild(imageRoom1);
 
-      /* === SETUP TEXT MESSAGE === */
+      /* === SETUP TEXT MESSAGES === */
       const message1 = textMessage('I want to show you my room');
+      app.stage.addChild(message1);
       const message2 = textMessage('I will tell you about my room');
-
+      app.stage.addChild(message2);
 
       /* === SETUP WHITE FLASH === */
-      const flash = new PIXI.Graphics();
-      flash.beginFill(0xFFFFFF);
-      flash.drawRect(0, 0, 1920, 1080);
-      flash.endFill();
-      flash.alpha = 0;
+      const flash = placeFlash();
+      flash.displayGroup = topLayer;
       app.stage.addChild(flash);
 
       /* === SETUP DOOR SHAPE === */
@@ -77,15 +48,13 @@ class Canvas extends Component {
         pixi: { skewX: -30, scaleY: 12 },
       }, '+=0.04');
 
-      tl.to(flash, 0.2, {
-        alpha: 1,
-        onComplete: () => app.stage.removeChild(message1),
-      });
+      tl.to(flash, 0.2, { alpha: 1 });
+
+      tl.to(message1, 0, { alpha: 0 });
 
       tl.to(imageRoom1, 0, { pixi: { alpha: 1, x: -1920, y: 0 } });
       tl.to(flash, 0.08, { alpha: 0 });
       tl.to(imageRoom1, 2, { pixi: { x: -1160, y: -200 }, ease: Power1.easeOut });
-
 
       tl.to(doorShapePoints, 2, {
         ...[886, 1080, 515, 163, 960, 237, 1322, 1080, 886, 1080],
@@ -111,31 +80,33 @@ class Canvas extends Component {
       tl.to(message2, 0, { pixi: { skewX: 50, x: 600 } }, '+=0.04');
       tl.to(message2, 0, { pixi: { skewX: 0, x: 1320, y: 640 } }, '+=0.04');
 
-
-      const videoTexture = PIXI.VideoBaseTexture.fromUrl(videoFile);
-      videoTexture.autoPlay = false;
-      const texture = new PIXI.Texture(videoTexture);
-      const source = videoTexture.source;
-      const videoSprite = new PIXI.Sprite(texture);
-      videoSprite.alpha = 0;
-      app.stage.addChild(videoSprite);
+      //vid
+      const video1 = placeVideo(videoFile);
+      app.stage.addChild(video1.sprite);
 
       tl.to(flash, 0.2, {
         alpha: 1,
-        onComplete: function () {
+        onComplete: () => {
           app.stage.removeChild(message2);
         }
       });
-      tl.to(videoSprite, 0, {
+      tl.to(flash, 0.08, { alpha: 0 });
+      tl.to(video1.sprite, 0, {
         pixi: {
-          alpha: 1, onComplete: function () {
-            source.play();
+          alpha: 1,
+          onStart: () => {
+            tl.pause();
+          },
+          onComplete: () => {
+            video1.source.play();
+            video1.source.addEventListener('ended', () => tl.play());
           }
         }
       });
-      tl.to(flash, 0.08, { alpha: 0 });
+      tl.to(flash, 0.08, { alpha: 1 });
 
-    }
+
+    };
 
     const app = new PIXI.Application(1920, 1080, {
       backgroundColor: 0x000000,
---
+++ src/components/helpers/index.js
@@ -0,0 +1,57 @@
+import * as PIXI from 'pixi.js';
+
+const textMessage = (text) => {
+  const message = new PIXI.Text(
+    text,
+    {
+      fontFamily: 'TypeWriter',
+      fontSize: 64,
+      fill: 'white',
+    },
+  );
+
+  message.anchor.x = 0.5;
+  message.anchor.y = 0.5;
+  message.x = 1320;
+  message.y = 540;
+  message.alpha = 0;
+  return message;
+};
+
+const moveAndLineTo = (shape, points) => {
+  shape.clear();
+  shape.beginFill(0xFFFFFF);
+  shape.moveTo(points[0], points[1]);
+  for (let i = 2; i < points.length; i = i + 2) {
+    shape.lineTo(points[i], points[i + 1]);
+  }
+};
+
+const placeFlash = () => {
+  const flash = new PIXI.Graphics();
+  flash.beginFill(0xFFFFFF);
+  flash.drawRect(0, 0, 1920, 1080);
+  flash.endFill();
+  flash.alpha = 0;
+  return flash;
+};
+
+const placeVideo = (videoFile) => {
+  const videoTexture = PIXI.VideoBaseTexture.fromUrl(videoFile);
+  videoTexture.autoPlay = false;
+  const texture = new PIXI.Texture(videoTexture);
+  const source = videoTexture.source;
+  const sprite = new PIXI.Sprite(texture);
+  sprite.alpha = 0;
+  return {
+    sprite,
+    source,
+  };
+};
+
+export {
+  textMessage,
+  moveAndLineTo,
+  placeFlash,
+  placeVideo,
+};

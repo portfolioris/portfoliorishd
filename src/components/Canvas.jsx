25bf2966d3be9dbe3a3055a15d0f7cd528e2ee41
Hulsbosch <j.hulsbosch@colours.nl>
2017-11-16 21:56:57 +0100

    add some images

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { TimelineMax, Linear, Power1, morphSVG } from 'gsap';
+import { TimelineMax, Linear, Power1 } from 'gsap';
 import * as PIXI from 'pixi.js';
 import 'gsap/PixiPlugin';
 import 'pixi-display';
 import { moveAndLineTo, textMessage, placeFlash, placeVideo } from './helpers';
 
 import './Canvas.css';
-import imageFile from './../images/intro_1.jpg';
-import videoFile from './../images/intro_seq_1.mp4';
+import imageIntro1 from './../images/intro_1.jpg';
+import imageIntro2 from './../images/intro_2.jpg';
+import imageHiddenMe1 from './../images/hidden-me-1.jpg';
+import videoIntro1 from './../images/intro_seq_1.mp4';
 
 class Canvas extends Component {
   componentDidMount() {
@@ -16,16 +18,26 @@ class Canvas extends Component {
       app.stage.displayList = new PIXI.DisplayList();
       const topLayer = new PIXI.DisplayGroup(1, true);
 
-      /* === SETUP FIRST IMAGE === */
-      const imageRoom1 = new PIXI.Sprite(resources.imageRoom1.texture);
-      imageRoom1.alpha = 0;
-      app.stage.addChild(imageRoom1);
+      /* === SETUP IMAGES === */
+      const imageIntro1 = new PIXI.Sprite(resources.imageIntro1.texture);
+      imageIntro1.alpha = 0;
+      app.stage.addChild(imageIntro1);
+
+      const imageIntro2 = new PIXI.Sprite(resources.imageIntro2.texture);
+      imageIntro2.alpha = 0;
+      app.stage.addChild(imageIntro2);
+
+      const imageHiddenMe1 = new PIXI.Sprite(resources.imageHiddenMe1.texture);
+      imageHiddenMe1.alpha = 0;
+      app.stage.addChild(imageHiddenMe1);
 
       /* === SETUP TEXT MESSAGES === */
       const message1 = textMessage('I want to show you my room');
       app.stage.addChild(message1);
       const message2 = textMessage('I will tell you about my room');
       app.stage.addChild(message2);
+      const message3 = textMessage('I\'ll pretend I\'m not here');
+      app.stage.addChild(message3);
 
       /* === SETUP WHITE FLASH === */
       const flash = placeFlash();
@@ -35,7 +47,7 @@ class Canvas extends Component {
       /* === SETUP DOOR SHAPE === */
       const doorShape = new PIXI.Graphics();
       app.stage.addChild(doorShape);
-      imageRoom1.mask = doorShape;
+      imageIntro1.mask = doorShape;
 
       const doorShapePoints = [188, 1080, 0, 616, 170, 889, 246, 1080, 188, 1080];
       moveAndLineTo(doorShape, doorShapePoints);
@@ -52,9 +64,9 @@ class Canvas extends Component {
 
       tl.to(message1, 0, { alpha: 0 });
 
-      tl.to(imageRoom1, 0, { pixi: { alpha: 1, x: -1920, y: 0 } });
+      tl.to(imageIntro1, 0, { pixi: { alpha: 1, x: -1920, y: 0 } });
       tl.to(flash, 0.08, { alpha: 0 });
-      tl.to(imageRoom1, 2, { pixi: { x: -1160, y: -200 }, ease: Power1.easeOut });
+      tl.to(imageIntro1, 2, { pixi: { x: -1160, y: -200 }, ease: Power1.easeOut });
 
       tl.to(doorShapePoints, 2, {
         ...[886, 1080, 515, 163, 960, 237, 1322, 1080, 886, 1080],
@@ -62,7 +74,7 @@ class Canvas extends Component {
         onUpdateParams: [doorShape, doorShapePoints],
       }, "-=2");
 
-      tl.to(imageRoom1, 3, { pixi: { x: 0, y: -1080 }, ease: Power1.easeInOut });
+      tl.to(imageIntro1, 3, { pixi: { x: 0, y: -1080 }, ease: Power1.easeInOut });
       tl.to(doorShapePoints, 3, {
         ...[125, 1080, -167, 270, 1632, -369, 2205, 1080],
         onUpdate: moveAndLineTo,
@@ -70,7 +82,7 @@ class Canvas extends Component {
         ease: Power1.easeInOut
       }, "-=3");
 
-      tl.to(imageRoom1, 0, { pixi: { alpha: 0 } });
+      tl.to(imageIntro1, 0, { pixi: { alpha: 0 } });
       tl.to(message2, 0, { pixi: { alpha: 1, skewX: -60, x: 600 } });
       tl.to(message2, 0, { pixi: { skewX: 0, x: 1320, scaleY: 1.5 } }, '+=0.04');
       tl.to(message2, 0, { pixi: { scaleY: 1 } }, '+=0.04');
@@ -81,7 +93,7 @@ class Canvas extends Component {
       tl.to(message2, 0, { pixi: { skewX: 0, x: 1320, y: 640 } }, '+=0.04');
 
       //vid
-      const video1 = placeVideo(videoFile);
+      const video1 = placeVideo(videoIntro1);
       app.stage.addChild(video1.sprite);
 
       tl.to(flash, 0.2, {
@@ -104,8 +116,9 @@ class Canvas extends Component {
         }
       });
       tl.to(flash, 0.08, { alpha: 1 });
-
-
+      tl.to(message3, 0, { pixi: { alpha: 1 } });
+      tl.from(message3, 2, { pixi: { scale: 0.9 }, ease: Linear.easeNone });
+      tl.from(imageHiddenMe1, 0.5, { pixi: { alpha: 1 }, ease: Linear.easeNone });
     };
 
     const app = new PIXI.Application(1920, 1080, {
@@ -115,8 +128,10 @@ class Canvas extends Component {
     });
 
     const loader = PIXI.loader;
-    loader.add('imageRoom1', imageFile);
-    loader.add('video1', videoFile);
+    loader.add('imageIntro1', imageIntro1);
+    loader.add('imageIntro2', imageIntro2);
+    loader.add('imageHiddenMe1', imageHiddenMe1);
+    loader.add('videoIntro1', videoIntro1);
     loader.load(onAssetsLoaded);
   }
 
